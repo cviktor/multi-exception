ec0b6f79f7c9e5fedffaaa6e69365c9f8ac0504a
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-10-08 17:49:08 +0200

    Első try-catch

--- Exception/Program.cs
+++ Exception/Program.cs
@@ -11,12 +11,33 @@ namespace Exception
         static void Main(string[] args)
         {
             Console.WriteLine("Kérek egy számot");
-            string line = Console.ReadLine();
-            double number = double.Parse(line);
-            Console.WriteLine("A szám 10szerese: " + number * 10);
+
+            try
+            {
+                string line = Console.ReadLine();
+                int number = MyParse(line);
+                Console.WriteLine("A szám 10szerese: " + number * 10);
+            }
+            catch
+            {
+                Console.WriteLine("Hiba történt");
+            }
+
             Console.ReadKey();
         }
 
-        
+        static int MyParse(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!char.IsDigit(text[i]))
+                {
+                    throw new System.Exception("Hibás karakter " + text[i]);
+                }
+            }
+
+            return int.Parse(text); //biztos működni fog
+        }
+
     }
 }
