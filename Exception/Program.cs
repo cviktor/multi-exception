8b7d750987cd43b5a4fab4c7d589acf2dc742c9b
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-10-08 17:32:15 +0200

    Kivétel első példa int.Parse-al

--- Exception/Program.cs
+++ Exception/Program.cs
@@ -10,6 +10,13 @@ namespace Exception
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Kérek egy számot");
+            string line = Console.ReadLine();
+            double number = double.Parse(line);
+            Console.WriteLine("A szám 10szerese: " + number * 10);
+            Console.ReadKey();
         }
+
+        
     }
 }
